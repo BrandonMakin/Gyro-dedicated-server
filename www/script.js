6bf18c004c4c29bcab3d2d86162b1470fbf64a02
Brandon Makin <bmicmak@gmail.com>
2019-05-04 16:22:21 -0700

    added color schemes in browser

--- www/script.js
+++ www/script.js
@@ -3,6 +3,19 @@ let gameID;
 let sendMessages = false;
 const socket = io({autoConnect: false});
 
+var color_schemes = [
+	["#f59aa9", "#c74257", "#870b1f"],
+	["#4d3ab5", "#2e16b1", "#190a6b"],
+	["#ffff63", "#ffff00", "#c5c500"],
+	["#4abd9d", "#00a779", "#006549"],
+	["#e0a8ff", "#cf78ff", "#a90dff"],
+	["#c0ffff", "#00ffff", "#008282"],
+	["#ffffff", "#e7e7e7", "#aeaeae"],
+	["#444444", "#222222", "#000000"],
+	["#ff5c00", "#c54700", "#9b3800"],
+	["#9feb5b", "#6be400", "#418a00"]
+]
+
 window.addEventListener("deviceorientation", onMotion, true);
 
 function joinGame() {
@@ -25,6 +38,13 @@ socket.on('nonexistent_game', () => {
   socket.emit("gameid", gameID);
   sendMessages = true;
 })
+
+socket.on('color_scheme', set_color_scheme)
+function set_color_scheme(scheme_id) {
+  document.getElementById("b1").style.background = color_schemes[scheme_id][0]
+  document.getElementById("b2").style.background = color_schemes[scheme_id][1]
+  document.getElementById("b3").style.background = color_schemes[scheme_id][2]
+}
 
 function bStart(bname) {
   socket.emit(bname, "on")
